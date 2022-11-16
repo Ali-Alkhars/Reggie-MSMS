79c0dcb0109940b8c7b902d3ecf60168cc8633bb
Ali Alkhars <a-khars@hotmail.com>
2022-11-16 12:20:57 +0000

    Create User model and User migrations
    - Created a user model
    - Added unit tests for the model
    - Added user fixtures for testing
    - Did database migrations for the model
    - Changed some file structure for tests

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """User model used for creating different users in the MSMS."""
+
+    username = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex='[a-z0-9]+@[a-z]+\.[a-z]{2,3}',
+                message='Username must be an Email',
+                code='invalid_username'
+            )
+        ]
+    )
+    first_name = models.CharField(max_length=20, blank=False)
+    last_name = models.CharField(max_length=20, blank=False)
