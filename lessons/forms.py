2ad3985c4c1afe59afc689075e7bd83b23a192e2
iliajamasb <99290474+iliajamasb@users.noreply.github.com>
2022-11-17 10:51:29 +0000

    Created basic form login/signup functionality

---
+++ lessons/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class LogInForm(forms.Form):
+    username = forms.CharField(label="Username")
+    password = forms.CharField(label="Password", widget=forms.PasswordInput())
+
+class SignUpForm(forms.Form):
+    print("test")
--- lessons/views.py
+++ lessons/views.py
@@ -1,4 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LogInForm, SignUpForm
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, 'home.html')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('main')
+    else:
+        form = SignUpForm()
+    return render(request, 'sign_up.html', {'form': form})
+
+def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('main')
+        messages.add_message(request, messages.ERROR, "Invalid credentials")
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form})
+
+def main(request):
+    return render(request, 'main.html', {'main': main})
