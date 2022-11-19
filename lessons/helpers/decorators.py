a2750f185ae19b695488a549891dbc41cb04a672
Ali Alkhars <a-khars@hotmail.com>
2022-11-19 17:41:13 +0000

    Add redirect route to @login_prohibited decorator

--- lessons/helpers/decorators.py
+++ lessons/helpers/decorators.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 def login_prohibited(view_function):
     def modified_view_function(request):
         if request.user.is_authenticated:
-            return redirect('')
+            return redirect(settings.REDIRECT_URL_WHEN_LOGGED_IN)
         else:
             return view_function(request)
     return modified_view_function
