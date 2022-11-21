5e26c98d8efbb91f65477f1c8c5932ea1f6b260a
Ali Alkhars <a-khars@hotmail.com>
2022-11-21 17:45:48 +0000

    Create admin configurations

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,12 @@
+"""Configuration of the admin interface for MSMS"""
 from django.contrib import admin
+from lessons.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for users."""
+
+    list_display = [
+        'username', 'first_name', 'last_name', 'is_active',
+    ]
