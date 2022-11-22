e3eab81bedd7ebfa0b1664f286c7eacac478a86a
Chin Wan <k21016106@kcl.ac.uk>
2022-11-22 20:06:07 +0000

    Finish implementation of lesson page

--- lessons/views.py
+++ lessons/views.py
@@ -4,6 +4,7 @@ from .models import Lesson_request
 from django.contrib import messages
 from lessons.helpers.decorators import login_prohibited, permitted_groups
 from django.contrib.auth.decorators import login_required
+from lessons.helpers.helper_functions import get_user_group
 
 
 # Create your views here.
@@ -36,12 +37,26 @@ def lesson_request(request):
 # @login_required
 # @permitted_groups(['student'])
 def lesson_page(request):
+    # isStudent = userOrAdmin(request);
+    # if (isStudent):
+        # user_lessons = Lesson_request.objects.filter(user=request.user)
+        # data = {'object_list': user_lessons}
+    # else:
+        # data = {'object_list': Lesson_request.objects.all()}
     data = {'object_list':Lesson_request.objects.all()}
     return render(request, "lesson_page.html", data);
 
 # @login_required
 # @permitted_groups(['admin', 'director'])
 def lesson_admin(request):
-    return render(request, "lesson_admin.html");
+    user_lessons = Lesson_request.objects.filter(user=request.user)
+    data = {'object_list':user_lessons}
+    return render(request, "lesson_admin.html", data);
+
+def userOrAdmin(request):
+    if (get_user_group(request) == 'student'):
+        return True
+    return False
+
 
     
