510622573a0e09725b3b6612bae2d13dc7ccf895
Abdirahman Ahmed <Abdirahman.ahmed@kcl.ac.uk>
2022-11-26 23:33:02 +0000

    test that valid log in by inactive user fails

--- lessons/tests/tests_log_in_view.py
+++ lessons/tests/tests_log_in_view.py
@@ -13,7 +13,9 @@ class LogInViewTestCase(TestCase, LogInTester):
 
     def setUp(self):
         self.url = reverse('log_in')
-        self.user = User.objects.get(username='johndoe@example.org')
+        self.user = User.objects.get(username='johndoe@example.org', 
+        is_active=True,
+        )
 
     def test_log_in_url(self):
         self.assertEqual(self.url,'/log_in/')
@@ -44,3 +46,16 @@ class LogInViewTestCase(TestCase, LogInTester):
         response_url = reverse('home')
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_valid_log_in_by_inactive_user(self):
+        self.user.is_active = False
+        self.user.save()
+        form_input = { 'username': 'johndoe@example.org', 'password': 'Password123' }
+        response = self.client.post(self.url, form_input, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, LogInForm))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self._is_logged_in())
+        
